e7ef76bbd3ed609d14ddecb05963463767fa0066
Ruveyda Hilal <rhakcinar@gmail.com>
2022-04-21 03:34:43 +0300

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -2,136 +2,136 @@
 
 /* Navigation*/
 // Collapse the navbar by adding the top-nav-collapse class
-window.onscroll = function () {
-	scrollFunction();
-	scrollFunctionBTT(); // back to top button
+window.onscroll = function() {
+    scrollFunction();
+    scrollFunctionBTT(); // back to top button
 };
 
-window.onload = function () {
-	scrollFunction();
+window.onload = function() {
+    scrollFunction();
 };
 
 function scrollFunction() {
-	if (document.documentElement.scrollTop > 30) {
-		document.getElementById("navbarExample").classList.add("top-nav-collapse");
-	} else if ( document.documentElement.scrollTop < 30 ) {
-		document.getElementById("navbarExample").classList.remove("top-nav-collapse");
-	}
+    if (document.documentElement.scrollTop > 30) {
+        document.getElementById("navbarExample").classList.add("top-nav-collapse");
+    } else if (document.documentElement.scrollTop < 30) {
+        document.getElementById("navbarExample").classList.remove("top-nav-collapse");
+    }
 }
 
 // Navbar on mobile
 let elements = document.querySelectorAll(".nav-link:not(.dropdown-toggle)");
 
 for (let i = 0; i < elements.length; i++) {
-	elements[i].addEventListener("click", () => {
-		document.querySelector(".offcanvas-collapse").classList.toggle("open");
-	});
+    elements[i].addEventListener("click", () => {
+        document.querySelector(".offcanvas-collapse").classList.toggle("open");
+    });
 }
 
 document.querySelector(".navbar-toggler").addEventListener("click", () => {
-  	document.querySelector(".offcanvas-collapse").classList.toggle("open");
+    document.querySelector(".offcanvas-collapse").classList.toggle("open");
 });
 
 // Hover on desktop
 function toggleDropdown(e) {
-	const _d = e.target.closest(".dropdown");
-	let _m = document.querySelector(".dropdown-menu", _d);
+    const _d = e.target.closest(".dropdown");
+    let _m = document.querySelector(".dropdown-menu", _d);
 
-	setTimeout(
-		function () {
-		const shouldOpen = _d.matches(":hover");
-		_m.classList.toggle("show", shouldOpen);
-		_d.classList.toggle("show", shouldOpen);
+    setTimeout(
+        function() {
+            const shouldOpen = _d.matches(":hover");
+            _m.classList.toggle("show", shouldOpen);
+            _d.classList.toggle("show", shouldOpen);
 
-		_d.setAttribute("aria-expanded", shouldOpen);
-		},
-		e.type === "mouseleave" ? 300 : 0
-	);
+            _d.setAttribute("aria-expanded", shouldOpen);
+        },
+        e.type === "mouseleave" ? 300 : 0
+    );
 }
 
 // On hover
 const dropdownCheck = document.querySelector('.dropdown');
 
-if (dropdownCheck !== null) { 
-	document.querySelector(".dropdown").addEventListener("mouseleave", toggleDropdown);
-	document.querySelector(".dropdown").addEventListener("mouseover", toggleDropdown);
+if (dropdownCheck !== null) {
+    document.querySelector(".dropdown").addEventListener("mouseleave", toggleDropdown);
+    document.querySelector(".dropdown").addEventListener("mouseover", toggleDropdown);
 
-	// On click
-	document.querySelector(".dropdown").addEventListener("click", (e) => {
-		const _d = e.target.closest(".dropdown");
-		let _m = document.querySelector(".dropdown-menu", _d);
-		if (_d.classList.contains("show")) {
-			_m.classList.remove("show");
-			_d.classList.remove("show");
-		} else {
-			_m.classList.add("show");
-			_d.classList.add("show");
-		}
-	});
+    // On click
+    document.querySelector(".dropdown").addEventListener("click", (e) => {
+        const _d = e.target.closest(".dropdown");
+        let _m = document.querySelector(".dropdown-menu", _d);
+        if (_d.classList.contains("show")) {
+            _m.classList.remove("show");
+            _d.classList.remove("show");
+        } else {
+            _m.classList.add("show");
+            _d.classList.add("show");
+        }
+    });
 }
-  
+
 
 /* Card Slider - Swiper */
 var cardSlider = new Swiper('.card-slider', {
-	autoplay: {
-		delay: 5000,
-		disableOnInteraction: false
-	},
-	loop: true,
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev'
-	},
-	slidesPerView: 2,
-	spaceBetween: 70,
-	breakpoints: {
-		// when window is <= 991px
-		991: {
-			slidesPerView: 1
-		}
-	}
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    slidesPerView: 2,
+    spaceBetween: 70,
+    breakpoints: {
+        // when window is <= 991px
+        991: {
+            slidesPerView: 1
+        }
+    }
 });
 
 
 /* Filter - Isotope */
 const gridCheck = document.querySelector('.grid');
 
-if (gridCheck !== null) { 
-	// init Isotope
-	var iso = new Isotope( '.grid', {
-		itemSelector: '.element-item',
-		layoutMode: 'fitRows'
-	});
+if (gridCheck !== null) {
+    // init Isotope
+    var iso = new Isotope('.grid', {
+        itemSelector: '.element-item',
+        layoutMode: 'fitRows'
+    });
 
-	// bind filter button click
-	var filtersElem = document.querySelector('.filters-button-group');
-	filtersElem.addEventListener( 'click', function( event ) {
-		// only work with buttons
-		if ( !matchesSelector( event.target, 'button' ) )  {
-			return;
-		}
-		var filterValue = event.target.getAttribute('data-filter');
-		// use matching filter function
-		iso.arrange({ filter: filterValue });
-	});
-	
-	// change is-checked class on buttons
-	var buttonGroups = document.querySelectorAll('.button-group');
-	for ( var i=0, len = buttonGroups.length; i < len; i++ ) {
-		var buttonGroup = buttonGroups[i];
-		radioButtonGroup( buttonGroup );
-	}
-	
-	function radioButtonGroup( buttonGroup ) {
-		buttonGroup.addEventListener( 'click', function( event ) {
-			// only work with buttons
-			if ( !matchesSelector( event.target, 'button' ) )  {
-				return;
-			}
-			buttonGroup.querySelector('.is-checked').classList.remove('is-checked');
-			event.target.classList.add('is-checked');
-		});
-	}
+    // bind filter button click
+    var filtersElem = document.querySelector('.filters-button-group');
+    filtersElem.addEventListener('click', function(event) {
+        // only work with buttons
+        if (!matchesSelector(event.target, 'button')) {
+            return;
+        }
+        var filterValue = event.target.getAttribute('data-filter');
+        // use matching filter function
+        iso.arrange({ filter: filterValue });
+    });
+
+    // change is-checked class on buttons
+    var buttonGroups = document.querySelectorAll('.button-group');
+    for (var i = 0, len = buttonGroups.length; i < len; i++) {
+        var buttonGroup = buttonGroups[i];
+        radioButtonGroup(buttonGroup);
+    }
+
+    function radioButtonGroup(buttonGroup) {
+        buttonGroup.addEventListener('click', function(event) {
+            // only work with buttons
+            if (!matchesSelector(event.target, 'button')) {
+                return;
+            }
+            buttonGroup.querySelector('.is-checked').classList.remove('is-checked');
+            event.target.classList.add('is-checked');
+        });
+    }
 }
 
 
@@ -141,15 +141,344 @@ myButton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 function scrollFunctionBTT() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		myButton.style.display = "block";
-	} else {
-		myButton.style.display = "none";
-	}
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myButton.style.display = "block";
+    } else {
+        myButton.style.display = "none";
+    }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-	document.body.scrollTop = 0; // for Safari
-	document.documentElement.scrollTop = 0; // for Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0; // for Safari
+    document.documentElement.scrollTop = 0; // for Chrome, Firefox, IE and Opera
 }
+
+/**
+ * WEBSITE: https://themefisher.com
+ * TWITTER: https://twitter.com/themefisher
+ * FACEBOOK: https://www.facebook.com/themefisher
+ * GITHUB: https://github.com/themefisher/
+ */
+
+(function($) {
+    'use strict';
+
+
+
+    // Sticky Menu
+    $(window).scroll(function() {
+        if ($('.navigation').offset().top > 100) {
+            $('.navigation').addClass('nav-bg');
+        } else {
+            $('.navigation').removeClass('nav-bg');
+        }
+    });
+
+    // Background-images
+    $('[data-background]').each(function() {
+        $(this).css({
+            'background-image': 'url(' + $(this).data('background') + ')'
+        });
+    });
+
+    // background color
+    $('[data-color]').each(function() {
+        $(this).css({
+            'background-color': $(this).data('color')
+        });
+    });
+
+    // progress bar
+    $('[data-progress]').each(function() {
+        $(this).css({
+            'bottom': $(this).data('progress')
+        });
+    });
+
+
+    /* ########################################### hero parallax ############################################## */
+    window.onload = function() {
+
+        var parallaxBox = document.getElementById('parallax');
+        var
+        /* c1left = document.getElementById('l1').offsetLeft,
+                   c1top = document.getElementById('l1').offsetTop, */
+            c2left = document.getElementById('l2').offsetLeft,
+            c2top = document.getElementById('l2').offsetTop,
+            c3left = document.getElementById('l3').offsetLeft,
+            c3top = document.getElementById('l3').offsetTop,
+            c4left = document.getElementById('l4').offsetLeft,
+            c4top = document.getElementById('l4').offsetTop,
+            c5left = document.getElementById('l5').offsetLeft,
+            c5top = document.getElementById('l5').offsetTop,
+            c6left = document.getElementById('l6').offsetLeft,
+            c6top = document.getElementById('l6').offsetTop,
+            c7left = document.getElementById('l7').offsetLeft,
+            c7top = document.getElementById('l7').offsetTop,
+            c8left = document.getElementById('l8').offsetLeft,
+            c8top = document.getElementById('l8').offsetTop,
+            c9left = document.getElementById('l9').offsetLeft,
+            c9top = document.getElementById('l9').offsetTop;
+
+        parallaxBox.onmousemove = function(event) {
+            event = event || window.event;
+            var x = event.clientX - parallaxBox.offsetLeft,
+                y = event.clientY - parallaxBox.offsetTop;
+
+            /*  mouseParallax('l1', c1left, c1top, x, y, 5); */
+            mouseParallax('l2', c2left, c2top, x, y, 25);
+            mouseParallax('l3', c3left, c3top, x, y, 20);
+            mouseParallax('l4', c4left, c4top, x, y, 35);
+            mouseParallax('l5', c5left, c5top, x, y, 30);
+            mouseParallax('l6', c6left, c6top, x, y, 45);
+            mouseParallax('l7', c7left, c7top, x, y, 30);
+            mouseParallax('l8', c8left, c8top, x, y, 25);
+            mouseParallax('l9', c9left, c9top, x, y, 40);
+        };
+
+    };
+
+    function mouseParallax(id, left, top, mouseX, mouseY, speed) {
+        var obj = document.getElementById(id);
+        var parentObj = obj.parentNode,
+            containerWidth = parseInt(parentObj.offsetWidth),
+            containerHeight = parseInt(parentObj.offsetHeight);
+        obj.style.left = left - (((mouseX - (parseInt(obj.offsetWidth) / 2 + left)) / containerWidth) * speed) + 'px';
+        obj.style.top = top - (((mouseY - (parseInt(obj.offsetHeight) / 2 + top)) / containerHeight) * speed) + 'px';
+    }
+    /* ########################################### /hero parallax ############################################## */
+
+    // testimonial-slider
+    $('.testimonial-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        arrows: false,
+        adaptiveHeight: true
+    });
+
+
+    // clients logo slider
+    $('.client-logo-slider').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots: false,
+        arrows: false,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    // Shuffle js filter and masonry
+    var Shuffle = window.Shuffle;
+    var jQuery = window.jQuery;
+
+    var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
+        itemSelector: '.shuffle-item',
+        buffer: 1
+    });
+
+    jQuery('input[name="shuffle-filter"]').on('change', function(evt) {
+        var input = evt.currentTarget;
+        if (input.checked) {
+            myShuffle.filter(input.value);
+        }
+    });
+
+
+
+})(jQuery);
+/**
+ * WEBSITE: https://themefisher.com
+ * TWITTER: https://twitter.com/themefisher
+ * FACEBOOK: https://www.facebook.com/themefisher
+ * GITHUB: https://github.com/themefisher/
+ */
+
+(function($) {
+    'use strict';
+
+
+
+    // Sticky Menu
+    $(window).scroll(function() {
+        if ($('.navigation').offset().top > 100) {
+            $('.navigation').addClass('nav-bg');
+        } else {
+            $('.navigation').removeClass('nav-bg');
+        }
+    });
+
+    // Background-images
+    $('[data-background]').each(function() {
+        $(this).css({
+            'background-image': 'url(' + $(this).data('background') + ')'
+        });
+    });
+
+    // background color
+    $('[data-color]').each(function() {
+        $(this).css({
+            'background-color': $(this).data('color')
+        });
+    });
+
+    // progress bar
+    $('[data-progress]').each(function() {
+        $(this).css({
+            'bottom': $(this).data('progress')
+        });
+    });
+
+
+    /* ########################################### hero parallax ############################################## */
+    window.onload = function() {
+
+        var parallaxBox = document.getElementById('parallax');
+        var
+        /* c1left = document.getElementById('l1').offsetLeft,
+                   c1top = document.getElementById('l1').offsetTop, */
+            c2left = document.getElementById('l2').offsetLeft,
+            c2top = document.getElementById('l2').offsetTop,
+            c3left = document.getElementById('l3').offsetLeft,
+            c3top = document.getElementById('l3').offsetTop,
+            c4left = document.getElementById('l4').offsetLeft,
+            c4top = document.getElementById('l4').offsetTop,
+            c5left = document.getElementById('l5').offsetLeft,
+            c5top = document.getElementById('l5').offsetTop,
+            c6left = document.getElementById('l6').offsetLeft,
+            c6top = document.getElementById('l6').offsetTop,
+            c7left = document.getElementById('l7').offsetLeft,
+            c7top = document.getElementById('l7').offsetTop,
+            c8left = document.getElementById('l8').offsetLeft,
+            c8top = document.getElementById('l8').offsetTop,
+            c9left = document.getElementById('l9').offsetLeft,
+            c9top = document.getElementById('l9').offsetTop;
+
+        parallaxBox.onmousemove = function(event) {
+            event = event || window.event;
+            var x = event.clientX - parallaxBox.offsetLeft,
+                y = event.clientY - parallaxBox.offsetTop;
+
+            /*  mouseParallax('l1', c1left, c1top, x, y, 5); */
+            mouseParallax('l2', c2left, c2top, x, y, 25);
+            mouseParallax('l3', c3left, c3top, x, y, 20);
+            mouseParallax('l4', c4left, c4top, x, y, 35);
+            mouseParallax('l5', c5left, c5top, x, y, 30);
+            mouseParallax('l6', c6left, c6top, x, y, 45);
+            mouseParallax('l7', c7left, c7top, x, y, 30);
+            mouseParallax('l8', c8left, c8top, x, y, 25);
+            mouseParallax('l9', c9left, c9top, x, y, 40);
+        };
+
+    };
+
+    function mouseParallax(id, left, top, mouseX, mouseY, speed) {
+        var obj = document.getElementById(id);
+        var parentObj = obj.parentNode,
+            containerWidth = parseInt(parentObj.offsetWidth),
+            containerHeight = parseInt(parentObj.offsetHeight);
+        obj.style.left = left - (((mouseX - (parseInt(obj.offsetWidth) / 2 + left)) / containerWidth) * speed) + 'px';
+        obj.style.top = top - (((mouseY - (parseInt(obj.offsetHeight) / 2 + top)) / containerHeight) * speed) + 'px';
+    }
+    /* ########################################### /hero parallax ############################################## */
+
+    // testimonial-slider
+    $('.testimonial-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        arrows: false,
+        adaptiveHeight: true
+    });
+
+
+    // clients logo slider
+    $('.client-logo-slider').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots: false,
+        arrows: false,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    // Shuffle js filter and masonry
+    var Shuffle = window.Shuffle;
+    var jQuery = window.jQuery;
+
+    var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
+        itemSelector: '.shuffle-item',
+        buffer: 1
+    });
+
+    jQuery('input[name="shuffle-filter"]').on('change', function(evt) {
+        var input = evt.currentTarget;
+        if (input.checked) {
+            myShuffle.filter(input.value);
+        }
+    });
+
+
+
+})(jQuery);
